ed11d3fa105c75b99a2352a6c6f6d59f0afc958d
Amirka-Kh <Amirka-Kh>
2025-09-09 18:43:44 +0300

    Add quotes administration

--- quotes/admin.py
+++ quotes/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Quote
+
+
+@admin.register(Quote)
+class QuoteAdmin(admin.ModelAdmin):
+    list_display = ("id", "source", "short_text", "weight", "views", "likes", "dislikes", "created_at")
+    list_filter = ("source",)
+    search_fields = ("source", "text")
+
+    def short_text(self, obj: Quote) -> str:
+        return (obj.text or "")[:60]
+    short_text.short_description = "text"
