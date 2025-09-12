7990f8364ca1b7bf349cabc9af5da2e0d9f5f214
Amirka-Kh <Amirka-Kh>
2025-09-12 15:38:22 +0300

    Add gunicorn config for easy usage

---
+++ gunicorn.conf.py
@@ -0,0 +1,10 @@
+bind = "0.0.0.0:8000"
+workers = 3
+worker_class = "sync"
+max_requests = 1000
+max_requests_jitter = 100
+timeout = 120
+keepalive = 5
+accesslog = "-"
+errorlog = "-"
+loglevel = "info"
